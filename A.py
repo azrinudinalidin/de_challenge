d8f7ac3c50fb643c4e4a0d91cb75243ad3461822
azrinudinalidin <85074151+azrinudinalidin@users.noreply.github.com>
2021-05-31 12:08:37 +0800

    Create A.py

---
+++ A.py
@@ -0,0 +1,24 @@
+def main():
+    import re
+
+    #example text = '  abcde   abc123 12345 123.45   def456ghi   123abc456def   75.78  10101  20asde20  visit   malaysia2020    '
+
+    txt = input('Enter text: ')
+    print(txt)
+
+    cariString(txt)
+
+    file_name = input('Enter file name :')
+    saveFile = open(file_name,'w')
+    for i in f:
+        saveFile.write(i)
+        saveFile.write(',')
+    saveFile.close()
+
+def cariString(text):
+    f = re.findall(r'\d*\.\d+|\s*\w*\s+',txt)
+    print('f '+str(f))
+    return
+
+if __name__ == "__main__":
+    main()
